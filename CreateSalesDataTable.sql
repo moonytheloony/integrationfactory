eb7cbd6a3f68ececa4f36b49c002afc541696483
moonytheloony <rahulrai@live.com>
2015-10-24 23:27:10 +0530

    Added create sales data script

---
+++ CreateSalesDataTable.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[SalesData] (
+    [Id]        INT             IDENTITY (1, 1) NOT NULL,
+    [OrderDate] DATETIME        NOT NULL,
+    [Region]    NVARCHAR (50)   NOT NULL,
+    [Rep]       NVARCHAR (MAX)  NOT NULL,
+    [Item]      NVARCHAR (MAX)  NOT NULL,
+    [Units]     INT             NOT NULL,
+    [Unit Cost] DECIMAL (18, 2) NOT NULL,
+    [Total]     DECIMAL (18, 2) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
